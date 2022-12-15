7226530967b245def088c705e9e37edd1eae49d1
Nathenial Buckley <nathenialbuckley98@gmail.com>
2022-12-15 11:31:23 -0600

    Added functionality for login/registration

---
+++ src/views/ApplicationViews.js
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import { Login } from "../componets/auth/Login"
+import { Register } from "../componets/auth/Register"
+import { Authorized } from "./AuthorizedViews"
+
+
+
+
+
+export const ApplicationViews = () => {
+    return <>
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route element={<Authorized />}>
+                {/* Add Routes here */}
+            </Route>
+        </Routes>
+    </>
+}
